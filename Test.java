780a3019d3df4e63d7c3a3548f919d3f23df90a1
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-04-07 00:23:41 -0700

    adding LinkedList implementation

--- Test.java
+++ Test.java
@@ -1,8 +1,21 @@
+
+
 public class Test {
     
     public static void main(String[] args) {
-        String x = "Testing the java";
-        System.out.println(x);
-        
+    	// Creating a LinkedList object
+    	LinkedList mylist = new LinkedList();
+    	
+    	mylist.InsertNode(6);
+    	mylist.InsertNode(2);
+    	mylist.InsertNode(5);
+    	mylist.InsertNode(10);
+    	mylist.InsertNode(3);
+    	
+    	mylist.InsertAt(2, 9);
+    	//mylist.InsertAtStart(77);
+    	//mylist.DeleteNode(10);
+    	mylist.PrintList();
+    	
     }
 }
