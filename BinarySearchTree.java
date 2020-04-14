1dabee2b5a09863581576ba8cb3916a206e52a86
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-04-14 11:38:05 -0700

    testing the tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -7,7 +7,7 @@ public class BinarySearchTree {
 	public BinarySearchTree() {
 		root = null;
 	}
-	
+	// This method inserts notes to the tree
 	public void insert(Node current, int value) {
         if (value < current.value) { 
         	if (current.left != null) { 
@@ -30,12 +30,29 @@ public class BinarySearchTree {
 	public void traverseInOrder(Node node) {
         if (node != null) {
             traverseInOrder(node.left);
-            System.out.print(" " + node.value);
+            System.out.print(node.value + " ");
             traverseInOrder(node.right);
         }
      }
 
+	public void traversePreOrder(Node node) {	
+		if (node != null) {
+			// First, print the data of the node
+			System.out.print(node.value + " ");
+			// recurse on the left subtree
+			traversePreOrder(node.left);
+			// recurse on right subtree
+			traversePreOrder(node.right);
+		}
+	}
 	
+	public void traversePostOrder(Node node) {
+		if (node != null) {
+			traversePostOrder(node.left);
+			traversePostOrder(node.right);
+			System.out.print(node.value + " ");
+		}
+	}
 	
 	
 	
--- Test.java
+++ Test.java
@@ -18,22 +18,34 @@ public class Test {
     	
     	//mylist.PrintList();
     	
+    	
+    	
+    	
     	// Binary Search Tree
     	BinarySearchTree tree = new BinarySearchTree();
-        Node root = new Node(5);
+        Node root = new Node(7);
         
         System.out.println("Building tree with root value " + root.value);
-        tree.insert(root, 2);
+        tree.insert(root, 5);
         tree.insert(root, 4);
-        tree.insert(root, 8);
         tree.insert(root, 6);
-        tree.insert(root, 7);
-        tree.insert(root, 3);
         tree.insert(root, 9);
+        tree.insert(root, 8);
+        tree.insert(root, 10);
+        tree.insert(root, 2);
+        System.out.println(" ");
         
         System.out.println("Traversing tree in order");
         tree.traverseInOrder(root);
-    	
+        System.out.println(" ");
+        
+        System.out.println("Traversing tree in pre order");
+        tree.traversePreOrder(root);
+        System.out.println(" ");
+        
+        System.out.println("Traversing tree in post order");
+        tree.traversePostOrder(root);
+        System.out.println(" ");
     	
     }
 }
